1c7b5616b82d21101369d263c41a59fb90cf3bec
dijalmacavalcante <dijalma0412@gmail.com>
2024-09-29 15:28:36 -0300

    chore: add novos icones e os estrutura numa pasta unica

--- src/social/component.jsx
+++ src/social/component.jsx
@@ -1,6 +1,11 @@
 import { html } from '@bake-js/-o-id/dom';
-import instagramImage from './instagramBranco.png'
-import spotifyImage from './spotifyBranco.png'
+import amazonIcon from './assets/amazon.png'
+import appleIcon from './assets/apple.png'
+import deezerIcon from './assets/deezer.png'
+import instagramImage from './assets/instagramBranco.png'
+import spotifyOriginal from './assets/spotify.png'
+import spotifyImage from './assets/spotifyBranco.png'
+import youtubeIcon from './assets/youtube.png'
 
 function component (self) {
   const instagram = html`
@@ -10,8 +15,38 @@ function component (self) {
   `
 
   const spotify = html`
-    <a class="social href="https://www.instagram.com/oficialaotopo/" target="_blank">
+    <a class="social" href="https://www.instagram.com/oficialaotopo/" target="_blank">
       <img class="social_image" src="${spotifyImage}" />
+    </a>
+  `
+
+  const amazon = html`
+    <a class="social" href="https://www.instagram.com/oficialaotopo/" target="_blank">
+      <img class="social_image" src="${amazonIcon}" />
+    </a>
+  `
+
+  const apple = html`
+    <a class="social" href="https://www.instagram.com/oficialaotopo/" target="_blank">
+      <img class="social_image" src="${appleIcon}" />
+    </a>
+  `
+
+  const deezer = html`
+    <a class="social" href="https://www.instagram.com/oficialaotopo/" target="_blank">
+      <img class="social_image" src="${deezerIcon}" />
+    </a>
+  `
+
+  const spotifyO = html`
+    <a class="social" href="https://www.instagram.com/oficialaotopo/" target="_blank">
+      <img class="social_image" src="${spotifyOriginal}" />
+    </a>
+  `
+
+  const youtube = html`
+    <a class="social" href="https://www.instagram.com/oficialaotopo/" target="_blank">
+      <img class="social_image" src="${youtubeIcon}" />
     </a>
   `
 
@@ -21,6 +56,21 @@ function component (self) {
 
     case "spotify":
       return spotify
+
+    case "apple":
+      return apple
+
+    case "amazon":
+      return amazon
+
+    case "deezer":
+      return deezer
+
+    case "spotifyOriginal":
+      return spotifyO
+
+    case "youtube":
+      return youtube
 
     default:
       return ""
